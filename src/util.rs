fd1d1381c243f6c4463a884a25bf41399a4d48c9
Federico Maria Morrone <contact@morrone.dev>
2023-02-20 13:59:38 +0100

    Added get_uname util fn

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,7 @@
+use std::mem::MaybeUninit;
+
 use clap::{crate_version, Command};
+use libc::{uname, utsname};
 
 pub fn new_command(name: &'static str, about: &'static str, multicall: bool) -> Command {
     Command::new(name)
@@ -15,4 +18,15 @@ macro_rules! define_command_bin {
             ::coreutils::commands::$command(::std::env::args(), false)
         }
     };
+}
+
+// #[cfg(feature = "libc")]
+pub fn get_uname() -> Option<utsname> {
+    let mut utsname = MaybeUninit::<utsname>::uninit();
+
+    if unsafe { uname(utsname.as_mut_ptr()) } == 0 {
+        Some(unsafe { utsname.assume_init() })
+    } else {
+        None
+    }
 }
