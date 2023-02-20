c58cc9935f2c480a6241da2f2f79fcba2b4797f4
Matilde Morrone <contact@morrone.dev>
2023-02-20 14:27:08 +0100

    Added uname command

--- src/commands.rs
+++ src/commands.rs
@@ -1,29 +1,33 @@
 #[cfg(feature = "arch")]
 mod arch;
+#[cfg(feature = "b2sum")]
+mod b2sum;
 #[cfg(feature = "basename")]
 mod basename;
 #[cfg(feature = "true")]
 mod r#true;
 #[cfg(feature = "tty")]
 mod tty;
+#[cfg(feature = "uname")]
+mod uname;
 #[cfg(feature = "who")]
 mod who;
 #[cfg(feature = "whoami")]
 mod whoami;
 #[cfg(feature = "yes")]
 mod yes;
-#[cfg(feature = "b2sum")]
-mod b2sum;
 
 #[cfg(feature = "arch")]
 pub use arch::arch;
+#[cfg(feature = "b2sum")]
+pub use b2sum::b2sum;
 #[cfg(feature = "true")]
 pub use r#true::r#true;
 #[cfg(feature = "tty")]
 pub use tty::tty;
+#[cfg(feature = "uname")]
+pub use uname::uname;
 #[cfg(feature = "whoami")]
 pub use whoami::whoami;
 #[cfg(feature = "yes")]
 pub use yes::yes;
-#[cfg(feature = "b2sum")]
-pub use b2sum::b2sum;
