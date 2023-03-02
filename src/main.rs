340266265f67ce1e497790946130a5a43fd80ed0
Matilde Morrone <contact@morrone.dev>
2023-03-02 23:30:17 +0100

    Simplify main with a macro

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,15 @@
-use std::process::ExitCode;
-
 use coreutils::{commands, Error, Result};
+use coreutils_macros::define_commands;
 
 fn main() -> Result {
     let mut args = std::env::args();
 
     if let Some(arg) = args.nth(1) {
-        match arg.as_str() {
-            #[cfg(feature = "arch")]
-            "arch" => commands::arch(args, true),
-            #[cfg(feature = "b2sum")]
-            "b2sum" => commands::b2sum(args, true),
-            #[cfg(feature = "base32")]
-            "base32" => commands::base32(args, true),
-            #[cfg(feature = "base64")]
-            "base64" => commands::base64(args, true),
-            #[cfg(feature = "basename")]
-            "basename" => commands::basename(args, true),
-            #[cfg(feature = "basenc")]
-            "basenc" => commands::basenc(args, true),
-            #[cfg(feature = "whoami")]
-            "whoami" => commands::whoami(args, true),
-            #[cfg(feature = "tty")]
-            "tty" => commands::tty(args, true),
-            #[cfg(feature = "false")]
-            "false" => Ok(ExitCode::FAILURE), // TODO: handle "--version" and "--help".
-            #[cfg(feature = "true")]
-            "true" => commands::r#true(args, true),
-            #[cfg(feature = "uname")]
-            "uname" => commands::uname(args, true),
-            #[cfg(feature = "uptime")]
-            "uptime" => commands::uptime(args, true),
-            #[cfg(feature = "yes")]
-            "yes" => commands::yes(args, true),
-            command => Err(Error::UnknownCommand(command.to_string())),
-        }
+        define_commands!(
+            arg, "arch", "b2sum", "base32", "base64", "basename", "basenc", "tty", "uname",
+            "uptime", "whoami", "yes"
+        )
     } else {
         Err(Error::NoCommand)
     }
 }
-
-// call_commands!{}
