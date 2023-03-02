3da3b65155c1fc7dca2d7eda2ce8983c558a45bd
Federico Maria Morrone <contact@morrone.dev>
2023-03-03 00:16:34 +0100

    Remove unnecessary macro

--- src/util.rs
+++ src/util.rs
@@ -11,15 +11,6 @@ pub fn new_command(name: &'static str, about: &'static str, multicall: bool) -> 
         .no_binary_name(multicall)
 }
 
-#[macro_export]
-macro_rules! define_command_bin {
-    ($command:ident) => {
-        fn main() -> ::coreutils::Result {
-            ::coreutils::commands::$command(::std::env::args(), false)
-        }
-    };
-}
-
 // #[cfg(feature = "libc")]
 pub fn get_uname() -> Result<utsname, IoError> {
     let mut utsname = MaybeUninit::<utsname>::uninit();
