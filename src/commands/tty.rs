fe0b63b71dbfb4dfd3837ac68601ea64ac692296
Federico Maria Morrone <contact@morrone.dev>
2023-04-03 23:56:57 +0200

    Updated tty to use rustix

--- src/commands/tty.rs
+++ src/commands/tty.rs
@@ -1,6 +1,10 @@
-use std::{env::Args, io::Write, os::unix::io::AsRawFd, process::ExitCode};
+use std::{
+    env::Args,
+    io::{stdout, Write},
+    process::ExitCode,
+};
 
-use nix::unistd::ttyname;
+use rustix::{io::stdout as rustix_stdout, termios::ttyname};
 
 use crate::{util::new_command, Result};
 
@@ -8,22 +12,22 @@ pub fn tty(args: Args) -> Result {
     new_command(
         "tty",
         "Print the file name of the terminal connected to standard input",
-        
     )
     .get_matches_from(args);
 
-    let mut stdout = std::io::stdout();
-    let raw_fd = stdout.as_raw_fd();
+    let name = ttyname(rustix_stdout(), Vec::new());
 
-    let name = ttyname(raw_fd);
+    let mut lock = stdout().lock();
 
     match name {
         Ok(name) => {
-            writeln!(stdout, "{}", name.display())?;
+            lock.write_all(name.to_bytes())?;
+            lock.write_all(b"\n")?;
+
             Ok(ExitCode::SUCCESS)
         }
         Err(_) => {
-            writeln!(stdout, "not a tty")?;
+            writeln!(lock, "not a tty")?;
             Ok(ExitCode::from(1))
         }
     }
