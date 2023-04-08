7a8c425d2d02ca21e909a0f51c12cf8e2b97c882
Federico Maria Morrone <contact@morrone.dev>
2023-04-08 23:39:09 +0200

    whoami: migrated command to lexopt

--- src/commands/whoami.rs
+++ src/commands/whoami.rs
@@ -5,17 +5,24 @@ use std::{
     process::ExitCode,
 };
 
+use lexopt::prelude::*;
 use rustix::process::geteuid;
 
-use crate::{util::new_command, Result};
+use crate::Result;
 
 pub fn whoami(args: Args) -> Result {
-    new_command(
-        "whoami",
-        "Print the user name associated with the current effective user ID",
-        
-    )
-    .get_matches_from(args);
+    let mut parser = lexopt::Parser::from_args(args);
+
+    while let Some(arg) = parser.next()? {
+        if let Long("help") = arg {
+            println!(
+                "Usage: whoami\nPrint the user name associated with the current effective user ID"
+            );
+            return Ok(ExitCode::SUCCESS);
+        } else {
+            return Err(arg.unexpected().into());
+        }
+    }
 
     let uid = geteuid();
     let pw = unsafe { libc::getpwuid(uid.as_raw()) };
