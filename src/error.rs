26a7a2bf800684fbb35ebd860fb79bddf7950546
Matilde Morrone <contact@morrone.dev>
2023-04-09 02:51:40 +0200

    Switch to thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,37 +1,17 @@
-
 use std::{fmt::Debug, io::Error as IoError, process::ExitCode};
 
 use lexopt::Error as ParseError;
 
-
 pub type Result<T = ExitCode, E = Error> = std::result::Result<T, E>;
 
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("No command specified")]
     NoCommand,
+    #[error("{0} is not a valid command")]
     UnknownCommand(String),
-    IoError(IoError),
-    ParseError(ParseError),
-}
-
-impl Debug for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::NoCommand => write!(f, "No command specified"),
-            Self::UnknownCommand(command) => write!(f, "{command} is not a valid command"),
-            Self::IoError(error) => error.fmt(f),
-            Self::ParseError(error) => error.fmt(f),
-        }
-    }
-}
-
-impl From<IoError> for Error {
-    fn from(error: IoError) -> Self {
-        Self::IoError(error)
-    }
-}
-
-impl From<ParseError> for Error {
-    fn from(error: ParseError) -> Self {
-        Self::ParseError(error)
-    }
+    #[error(transparent)]
+    IoError(#[from] IoError),
+    #[error(transparent)]
+    ParseError(#[from] ParseError),
 }
