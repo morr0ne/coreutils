e4868f4b3952760f123543dd6f5547af73c42d62
Matilde Morrone <contact@morrone.dev>
2023-04-09 02:51:22 +0200

    Added cksum command

--- src/commands/cksum.rs
+++ src/commands/cksum.rs
@@ -1,16 +1,11 @@
 use std::{env::Args, process::ExitCode};
 
-use crate::{util::new_command, Result};
+use crate::Result;
 
 // TODO: Implement command
 
 pub fn cksum(args: Args) -> Result {
-    let matches = new_command(
-        "cksum",
-        "Print or verify checksums",
-        
-    )
-    .get_matches_from(args);
+    println!("wow");
 
     Ok(ExitCode::SUCCESS)
 }
