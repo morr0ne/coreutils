5e65097aafe42cee2ca70e7b240ec146dc10fb28
Federico Maria Morrone <contact@morrone.dev>
2023-05-07 13:42:01 +0200

    Updated cksum command

--- src/commands/cksum.rs
+++ src/commands/cksum.rs
@@ -1,11 +1,22 @@
 use std::{env::Args, process::ExitCode};
 
+use lexopt::prelude::*;
+
 use crate::Result;
 
 // TODO: Implement command
 
 pub fn cksum(args: Args) -> Result {
-    println!("wow");
+    let mut parser = lexopt::Parser::from_args(args);
+
+    while let Some(arg) = parser.next()? {
+        if let Long("help") = arg {
+            println!("Usage: cksum\nPrint or verify checksums");
+            return Ok(ExitCode::SUCCESS);
+        } else {
+            return Err(arg.unexpected().into());
+        }
+    }
 
     Ok(ExitCode::SUCCESS)
 }
