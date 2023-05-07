53ac64c75b59558b49d46283a08cfe38dcf24286
Federico Maria Morrone <contact@morrone.dev>
2023-05-07 13:46:48 +0200

    Removed thiserror

--- src/error.rs
+++ src/error.rs
@@ -4,14 +4,32 @@ use lexopt::Error as ParseError;
 
 pub type Result<T = ExitCode, E = Error> = std::result::Result<T, E>;
 
-#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("No command specified")]
     NoCommand,
-    #[error("{0} is not a valid command")]
     UnknownCommand(String),
-    #[error(transparent)]
-    IoError(#[from] IoError),
-    #[error(transparent)]
-    ParseError(#[from] ParseError),
+    IoError(IoError),
+    ParseError(ParseError),
+}
+
+impl Debug for Error {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoCommand => write!(formatter, "No command specified"),
+            Self::UnknownCommand(command) => write!(formatter, "{command} is not a valid command"),
+            Self::IoError(error) => error.fmt(formatter),
+            Self::ParseError(error) => error.fmt(formatter),
+        }
+    }
+}
+
+impl From<IoError> for Error {
+    fn from(error: IoError) -> Self {
+        Self::IoError(error)
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(error: ParseError) -> Self {
+        Self::ParseError(error)
+    }
 }
