203232fd43eedba432e15dc7b501f5b80a217a08
Federico Maria Morrone <contact@morrone.dev>
2023-08-19 20:32:35 +0200

    Updated arch to lexopt_derive

--- src/commands/arch.rs
+++ src/commands/arch.rs
@@ -4,22 +4,16 @@ use std::{
     process::ExitCode,
 };
 
-use lexopt::prelude::*;
-use rustix::process::uname;
+use lexopt_derive::Parser;
+use rustix::system::uname;
 
 use crate::Result;
 
-pub fn arch(args: Args) -> Result {
-    let mut parser = lexopt::Parser::from_args(args);
+#[derive(Parser)]
+struct ParsedArgs {}
 
-    while let Some(arg) = parser.next()? {
-        if let Long("help") = arg {
-            println!("Usage: arch\nPrints the machine architecture");
-            return Ok(ExitCode::SUCCESS);
-        } else {
-            return Err(arg.unexpected().into());
-        }
-    }
+pub fn arch(args: Args) -> Result {
+    ParsedArgs::parse(args, "Usage: arch\nPrints the machine architecture")?;
 
     let mut lock = stdout().lock();
     lock.write_all(uname().machine().to_bytes())?;
